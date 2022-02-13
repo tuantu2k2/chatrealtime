3745c2c1d0d0b85cd393b1d952ee57caad07f4a8
Your Name <you@example.com>
2022-02-13 11:55:46 +0700

    f

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express()
-
+const path = require("path");
 const http = require('http')
 const server = http.createServer(app)
 const {Server} = require('socket.io')
 
 const io = new Server(server)
-
+app.use(express.static(path.join(__dirname,'public')))
 app.get('/',(req, res) =>{
     res.sendFile(__dirname+'/index.html')
 })
 
 io.on('connection',(socket)=>{
+
     console.log('user connected')
     socket.on('on-chat',data=>{
         io.emit('user-chat',data)
